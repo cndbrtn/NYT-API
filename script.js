44785803ad2477e08498843bd7ab459400152626
Cindy Burton <cindy.brton@gmail.com>
2019-11-23 13:53:16 -0700

    Wheeeeeee

---
+++ script.js
@@ -0,0 +1,10 @@
+// on click events to gather info from input fields to send to back end
+$("#search").on("click", function (event) {
+    
+    var userSearch = $("#terms").val();
+    var userNumber = $("#number-artcile").val();
+    var startYear = $("#start-year").val();
+    var endYear = $("end-year").val();
+    
+
+})
